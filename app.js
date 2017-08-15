214cb60fd10062a87f0cbd9a924fa95053c50d90
Catalin Stanciu <catacsdev@gmail.com>
2017-08-15 11:41:01 +0200

    Update code style

--- app.js
+++ app.js
@@ -4,26 +4,26 @@ const logger = require('./app/utils/logger.js');
 
 // get cpu info
 const CPUS = os.cpus();
-if (cluster.isMaster) {
 
+if (cluster.isMaster) {
     // create a worker for each cpu
     CPUS.forEach(() => cluster.fork());
  
-    cluster.on("listening", worker => {
-			logger.info("Cluster %d connected", worker.process.pid);
+    cluster.on('listening', worker => {
+	  logger.info('Cluster %d connected', worker.process.pid);
     });
  
-    cluster.on("disconnect", worker => {
-			logger.warn("Cluster %d disconnected", worker.process.pid);
+    cluster.on('disconnect', worker => {
+	  logger.warn('Cluster %d disconnected', worker.process.pid);
     });
  
-    cluster.on("exit", worker => {
-			logger.warn("Cluster %d is dead", worker.process.pid);
+    cluster.on('exit', worker => {
+	  logger.warn('Cluster %d is dead', worker.process.pid);
 
       // Ensure starts of a new cluster if an old one dies 
       cluster.fork();      
     });
  
 } else {
-    require("./server.js");
+    require('./server.js');
 }
